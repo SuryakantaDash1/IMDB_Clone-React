6e8727ddb09b7d8f134463ebb44bbbfb843ce917
Suryakanta Dash <sdash8273@gmail.com>
2023-07-21 16:00:00 +0530

    updated

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,11 +1,17 @@
 
 import { AppBar, Toolbar, styled,  } from '@mui/material';
 
+const StyledToolbar = styled(Toolbar)`
+    background: #121212;
+    min-height: 56px !important;
+`;
 
 const Header = () => {
     return (
         <AppBar>
-            <Toolbar></Toolbar>
+            <StyledToolbar>
+
+            </StyledToolbar>
         </AppBar>
     )
 }

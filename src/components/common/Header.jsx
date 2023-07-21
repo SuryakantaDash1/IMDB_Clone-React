a5865d058f977ab43f81aed9bc14f06db55fcdf0
Suryakanta Dash <sdash8273@gmail.com>
2023-07-21 15:52:37 +0530

    initial commit

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,13 @@
+
+import { AppBar, Toolbar, styled,  } from '@mui/material';
+
+
+const Header = () => {
+    return (
+        <AppBar>
+            <Toolbar></Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+import Header from "../components/common/Header";
+
+
+const Home = () => {
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+export default Home;

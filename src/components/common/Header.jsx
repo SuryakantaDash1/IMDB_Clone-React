290bbd66fc90d281abcb5143e0f1aebfc6e678f6
Suryakanta Dash <sdash8273@gmail.com>
2023-07-26 17:41:26 +0530

    updated

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,16 +1,23 @@
-
 import { AppBar, Toolbar, styled,  } from '@mui/material';
+import { logoURL } from '../../constants/constants';
+
+
 
 const StyledToolbar = styled(Toolbar)`
     background: #121212;
-    min-height: 56px !important;
+    min-height: 55px !important;
+    padding: 0 115px !important;
 `;
+
+const Logo = styled('img')({
+    width: 64
+})
 
 const Header = () => {
     return (
         <AppBar>
             <StyledToolbar>
-
+                <Logo src={logoURL} alt="logo" />
             </StyledToolbar>
         </AppBar>
     )

bc4dedcd123d42bd5c72834d6b86e5571400033a
Suryakanta Dash <sdash8273@gmail.com>
2023-07-26 17:42:16 +0530

    updated

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,7 +5,7 @@ import { logoURL } from '../../constants/constants';
 
 const StyledToolbar = styled(Toolbar)`
     background: #121212;
-    min-height: 55px !important;
+    min-height: 56px !important;
     padding: 0 115px !important;
 `;
 
